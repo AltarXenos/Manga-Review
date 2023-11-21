9132376829a134c405b1ea31662370f0e92bb251
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-21 15:19:20 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -1,12 +1,23 @@
-
 document.addEventListener('DOMContentLoaded', function() {
   var contentDiv = document.getElementById('content');
 
-  contentDiv.addEventListener('mouseover', function() {
-    contentDiv.style.backgroundImage = "url('Background numéro 1.jpg')";
+  // Fonction pour mettre à jour la taille de la div en fonction de la taille de la page
+  function updateSize() {
+    contentDiv.style.width = window.innerWidth + 'px';
+    contentDiv.style.height = window.innerHeight + 'px';
+  }
+
+  // Mettez à jour la taille initiale
+  updateSize();
+
+  // Attachez la fonction à l'événement de redimensionnement de la fenêtre
+  window.addEventListener('resize', updateSize);
+
+  contentDiv.addEventListener('mouseenter', function() {
+    contentDiv.style.backgroundImage = "url('chemin/vers/votre/image-alternative.jpg')";
   });
 
-  contentDiv.addEventListener('mouseout', function() {
-    contentDiv.style.backgroundImage = "url('Background numéro 2.jpg')";
+  contentDiv.addEventListener('mouseleave', function() {
+    contentDiv.style.backgroundImage = "url('chemin/vers/votre/image-initiale.jpg')";
   });
 });
