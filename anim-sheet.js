a29469b086d3a893ada2297f89417823f7103fdc
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-21 19:40:53 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -1,23 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Fonction pour mettre à jour la taille de la div en fonction de la taille de la page
-  function updateSize() {
-    var contentDiv = document.getElementById('content');
-  // Mettez à jour la taille initiale
-  updateSize();
-
-  // Attachez la fonction à l'événement de redimensionnement de la fenêtre
-  window.addEventListener('resize', updateSize);
-
   var contentDiv = document.getElementById('content');
 
-  // Vérifier si l'élément existe avant d'attacher les gestionnaires d'événements
+  // Attacher les gestionnaires d'événements seulement si l'élément existe
   if (contentDiv) {
     contentDiv.addEventListener('mouseenter', function() {
-      contentDiv.style.backgroundImage = "url('Background numéro 1.jpg')";
+      document.body.style.backgroundImage = "url('Background numéro 2.jpg')";
     });
 
     contentDiv.addEventListener('mouseleave', function() {
-      contentDiv.style.backgroundImage = "url('Background numéro 2.jpg')";
+      document.body.style.backgroundImage = "url('Background numéro 1.jpg')";
     });
   }
 });
