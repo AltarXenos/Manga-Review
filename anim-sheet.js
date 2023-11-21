edbaafb2154e3923197797ea1684a6c155f0cb9c
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-21 19:32:54 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -2,14 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // Fonction pour mettre à jour la taille de la div en fonction de la taille de la page
   function updateSize() {
     var contentDiv = document.getElementById('content');
-    
-    // Vérifier si l'élément existe
-    if (contentDiv) {
-      contentDiv.style.width = window.innerWidth + 'px';
-      contentDiv.style.height = window.innerHeight + 'px';
-    }
-  }
-
   // Mettez à jour la taille initiale
   updateSize();
 
@@ -21,11 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
   // Vérifier si l'élément existe avant d'attacher les gestionnaires d'événements
   if (contentDiv) {
     contentDiv.addEventListener('mouseenter', function() {
-      contentDiv.style.backgroundImage = "url('chemin/vers/votre/image-alternative.jpg')";
+      contentDiv.style.backgroundImage = "url('Background numéro 1.jpg')";
     });
 
     contentDiv.addEventListener('mouseleave', function() {
-      contentDiv.style.backgroundImage = "url('chemin/vers/votre/image-initiale.jpg')";
+      contentDiv.style.backgroundImage = "url('Background numéro 2.jpg')";
     });
   }
 });
