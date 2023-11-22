c8fc0179d7522d67948aa26410ceea277396fe11
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-22 11:30:46 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function changeBackground(imageUrl) {
-    document.body.style.backgroundImage = "url('"Background numéro 2"')";
-  }
+// Définir la fonction changeBackground en dehors de l'événement DOMContentLoaded
+function changeBackground(imageUrl) {
+  document.body.style.backgroundImage = "url(" + imageUrl + ")";
+}
 
+document.addEventListener('DOMContentLoaded', function() {
   var contentDiv = document.getElementById('content');
 
   // Attacher les gestionnaires d'événements seulement si l'élément existe
   if (contentDiv) {
     contentDiv.addEventListener('mouseenter', function() {
-      changeBackground('Background-numero-2.jpg');
+      changeBackground('Background numéro 2.jpg');
     });
 
     contentDiv.addEventListener('mouseleave', function() {
-      changeBackground('Background-numero-1.jpg');
+      changeBackground('Background numéro 1.jpg');
     });
   }
 });
