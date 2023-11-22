c01b296d9f9f185044a01ec5aae0429e281a0ec4
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-22 13:30:31 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -1,15 +1,10 @@
-// Attendez que le DOM soit prêt
 document.addEventListener("DOMContentLoaded", function () {
-  // Sélectionnez l'élément avec l'ID "animated-background"
   var animatedBackground = document.getElementById("animated-background");
 
-  // Ajoutez un écouteur d'événements pour l'événement "mouseover"
   animatedBackground.addEventListener("mouseover", function () {
-    // Changez l'image de fond lors du survol
-    animatedBackground.style.backgroundImage = "url('Background numéro 1')"; /* Remplacez 'image2.jpg' par le chemin de votre deuxième image */
+    animatedBackground.style.backgroundImage = "url('Background numéro 1.jpg')";
   });
 
-  // Ajoutez un écouteur d'événements pour l'événement "mouseout" (pour revenir à l'image d'origine)
   animatedBackground.addEventListener("mouseout", function () {
     animatedBackground.style.backgroundImage = "url('Background numéro 2.jpg')";
   });
