fc64ec73705319bda917d00b78c97f6f9a4754f0
Cluster <120667956+AltarXenos@users.noreply.github.com>
2023-11-22 18:42:13 +0100

    Update anim-sheet.js

--- anim-sheet.js
+++ anim-sheet.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
     animatedBackground.style.backgroundImage = "url('bg2.jpg')";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var textElement = document.getElementById("text");
+
+    // Ajoutez une classe pour appliquer l'effet d'apparition
+    textElement.classList.add("appear");
+
+    // Réglez le délai pour l'effet d'apparition (en millisecondes)
+    setTimeout(function () {
+        textElement.style.opacity = "1";
+    }, 500); // Ajoutez ou ajustez le délai selon vos préférences
+});
